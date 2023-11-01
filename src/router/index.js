9f7e849b3a01ebf44a9f99f8f2ddce1f4d25f5af
moyo <moyosxtouch.com>
2023-10-31 20:05:32 -0600

    inicia proyecto crm en vue

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Inicio from "../views/ClientesView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "inicio",
+      component: Inicio,
+    },
+    {
+      path: "/agregar-cliente",
+      name: "agregar-cliente",
+      component: () => import("../views/NuevoClienteView.vue"),
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/ClientesView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import RouterLink from "../components/UI/RouterLink.vue";
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="agregar-cliente">Agregar Cliente </RouterLink>
+    </div>
+    <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+  </div>
+</template>
